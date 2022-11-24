59938b41fede011d0f2e5fb1c60edb4ec6441f6f
Viktor Kirjanov <viktor.kirjanov@gmail.com>
2022-11-24 12:11:24 +0100

    feat:init product screen

---
+++ lib/screens/product_screen/product_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../../config/custom_theme.dart';
+
+class ProductScreen extends StatelessWidget {
+  const ProductScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomTheme.black3,
+      body: Container(),
+    );
+  }
+}
